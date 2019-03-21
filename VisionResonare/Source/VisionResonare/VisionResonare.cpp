b217d5817ca9cc845067227345617c4bb3129991
nullpointer-exception <bea.trice@outlook.de>
2019-03-21 12:54:26 +0100

    setting up

---
+++ VisionResonare/Source/VisionResonare/VisionResonare.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VisionResonare.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VisionResonare, "VisionResonare" );
---
+++ VisionResonare/Source/VisionResonare/VisionResonareGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VisionResonareGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VISIONRESONARE_API AVisionResonareGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
