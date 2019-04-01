dc843d456385ea0cc4eec8d236ac29f3afe240e7
nullpointer-exception <bea.trice@outlook.de>
2019-04-01 19:38:19 +0200

    no message

---
+++ VisionResonare/Source/VisionResonare/VRPlayer.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRPlayer.h"
+#include "Classes/Components/StaticMeshComponent.h"
+
+// Sets default values
+AVRPlayer::AVRPlayer()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* pRoot = CreateDefaultSubobject<USceneComponent>(TEXT("ROOT"));
+
+	RootComponent = pRoot;
+
+	LeftHand = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left Hand"));
+	LeftHand->SetupAttachment(RootComponent);
+
+	RightHand = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right Hand"));
+	RightHand->SetupAttachment(RootComponent);
+}
+
+bool AVRPlayer::HandleCollision(USceneComponent * OtherComp)
+{
+	if (OtherComp)
+	{
+		if (OtherComp != LeftHand && OtherComp != RightHand)
+		{
+			OtherComp->DestroyComponent();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// Called when the game starts or when spawned
+void AVRPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AVRPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ VisionResonare/Source/VisionResonare/VRPlayer.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "VRPlayer.generated.h"
+
+UCLASS()
+class VISIONRESONARE_API AVRPlayer : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AVRPlayer();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Player")
+		UStaticMeshComponent* LeftHand;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Player")
+		UStaticMeshComponent* RightHand;
+
+	UFUNCTION(BlueprintCallable, Category = "Player")
+		bool HandleCollision(USceneComponent* OtherComp);
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
