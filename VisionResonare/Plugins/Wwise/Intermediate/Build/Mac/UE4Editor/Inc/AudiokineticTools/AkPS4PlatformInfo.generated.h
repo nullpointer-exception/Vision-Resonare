c777ec35eb8db3f76fdb861b80f3d60a68ca7617
Lea <lea@schmidt-hlds.de>
2020-02-11 13:04:59 +0100

    Sounds_Werwolf

---
+++ VisionResonare/Plugins/Wwise/Intermediate/Build/Mac/UE4Editor/Inc/AudiokineticTools/AkPS4PlatformInfo.generated.h
@@ -0,0 +1,93 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef AUDIOKINETICTOOLS_AkPS4PlatformInfo_generated_h
+#error "AkPS4PlatformInfo.generated.h already included, missing '#pragma once' in AkPS4PlatformInfo.h"
+#endif
+#define AUDIOKINETICTOOLS_AkPS4PlatformInfo_generated_h
+
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_SPARSE_DATA
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_RPC_WRAPPERS
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_RPC_WRAPPERS_NO_PURE_DECLS
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUAkPS4PlatformInfo(); \
+	friend struct Z_Construct_UClass_UAkPS4PlatformInfo_Statics; \
+public: \
+	DECLARE_CLASS(UAkPS4PlatformInfo, UAkPlatformInfo, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AudiokineticTools"), NO_API) \
+	DECLARE_SERIALIZER(UAkPS4PlatformInfo)
+
+
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_INCLASS \
+private: \
+	static void StaticRegisterNativesUAkPS4PlatformInfo(); \
+	friend struct Z_Construct_UClass_UAkPS4PlatformInfo_Statics; \
+public: \
+	DECLARE_CLASS(UAkPS4PlatformInfo, UAkPlatformInfo, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AudiokineticTools"), NO_API) \
+	DECLARE_SERIALIZER(UAkPS4PlatformInfo)
+
+
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UAkPS4PlatformInfo(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UAkPS4PlatformInfo) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAkPS4PlatformInfo); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAkPS4PlatformInfo); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAkPS4PlatformInfo(UAkPS4PlatformInfo&&); \
+	NO_API UAkPS4PlatformInfo(const UAkPS4PlatformInfo&); \
+public:
+
+
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UAkPS4PlatformInfo(UAkPS4PlatformInfo&&); \
+	NO_API UAkPS4PlatformInfo(const UAkPS4PlatformInfo&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UAkPS4PlatformInfo); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UAkPS4PlatformInfo); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UAkPS4PlatformInfo)
+
+
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_PRIVATE_PROPERTY_OFFSET
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_6_PROLOG
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_PRIVATE_PROPERTY_OFFSET \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_SPARSE_DATA \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_RPC_WRAPPERS \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_INCLASS \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_PRIVATE_PROPERTY_OFFSET \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_SPARSE_DATA \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_RPC_WRAPPERS_NO_PURE_DECLS \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_INCLASS_NO_PURE_DECLS \
+	VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h_9_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> AUDIOKINETICTOOLS_API UClass* StaticClass<class UAkPS4PlatformInfo>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID VisionResonare_Plugins_Wwise_Source_AudiokineticTools_Classes_Platforms_AkPlatform_PS4_AkPS4PlatformInfo_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ VisionResonare/Plugins/Wwise/Intermediate/Build/Mac/UE4Editor/Inc/AudiokineticTools/AudiokineticToolsClasses.h
@@ -0,0 +1,29 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#pragma once
+
+
+#include "AudiokineticTools/Classes/ActorFactoryAkAmbientSound.h"
+#include "AudiokineticTools/Classes/AkAcousticTextureFactory.h"
+#include "AudiokineticTools/Classes/AkAudioBankFactory.h"
+#include "AudiokineticTools/Classes/AkAudioEventFactory.h"
+#include "AudiokineticTools/Classes/AkAuxBusFactory.h"
+#include "AudiokineticTools/Classes/AkJsonFactory.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_Android/AkAndroidPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_iOS/AkIOSPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_Linux/AkLinuxPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_Lumin/AkLuminPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_Mac/AkMacPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_PS4/AkPS4PlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_Switch/AkSwitchPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_Windows/AkWindowsPlatformInfo.h"
+#include "AudiokineticTools/Classes/Platforms/AkPlatform_XboxOne/AkXboxOnePlatformInfo.h"
+#include "AudiokineticTools/Classes/GenerateSoundBanksCommandlet.h"
+#include "AudiokineticTools/Classes/InterpTrackAkAudioEventHelper.h"
+#include "AudiokineticTools/Classes/InterpTrackAkAudioRTPCHelper.h"
+
